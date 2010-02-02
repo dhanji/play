a769d1c2d83eaa8ef474d56e904e785120601b38
Dhanji Prasanna <dhanji@dhcp-172-23-7-164.syd.corp.google.com>
2010-02-02 16:51:40 +1100

    angled stuff

--- try.hs
+++ try.hs
@@ -1,8 +1,4 @@
 puts str = putStrLn str
 
--- range function
-alphabet x1 x2 = if x1 == x2
-                   then [x2]
-                   else x1 : alphabet (succ x1) x2
-
-glom ls = [ st | st <- ls ]
+-- operational transform function
+insert st c index version = 
